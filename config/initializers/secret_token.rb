053a0831d3d7427347f97459074d6af3880e701b
Jake Parks <jake@veribuy.com>
2012-09-14 09:39:16 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'f56e1306f4784460b030ec3ffb0103b4a6bce77bf362c7615f550c401de3a03400f5c44a0f4c5daa55e5c55c666cc17c03aa31db5cc3d46d632ae09531d3f91a'
